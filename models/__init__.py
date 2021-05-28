175b982c8e643ffc590b732ac6719a0b548d696d
SamuelFelipe <2354@holbertonschool.com>
2021-05-28 17:16:46 -0500

    Upload - classes

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''Initialize the \'models\' package'''
+
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+storage = DataBase()
---
+++ models/employee.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3 '''Employee Class Definition''' from sqlalchemy import Column, String
+
+
+class Employee(Base):
+    '''
+Employee class.
+
+
+        Atributes:
+            id - emloyee identification
+            names - emloyee names
+            forename -emloyee forenames
+            job - emloyee job
+            worked_hours - hours in all the month
+
+        Methods:
+            health - calcule the health payment
+            pension - calculate the pension payment
+'''
+    pass
